57b1a14a27bb54e1cfc356e4dbc156c2f39986fe
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-02-03 19:59:30 +0700

    feat : add search button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello world!</h1>`,
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
